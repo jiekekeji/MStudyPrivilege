d67603fe32d295894812d655ba26c4f6dee5f932
jiekekeji <jieke_rose@foxmail.com>
2017-09-13 00:13:11 +0800

    init

--- src/main/java/com/jk/ctrl/TRoleCtrl.java
+++ src/main/java/com/jk/ctrl/TRoleCtrl.java
@@ -1,21 +1,31 @@
 package com.jk.ctrl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jk.mapper.TRoleMapper;
+import com.jk.pojo.TRole;
 
 @Controller
 @RequestMapping("/role")
 public class TRoleCtrl {
 
-	public TRoleCtrl() {
-		System.out.println("TRoleCtrl");
-	}
+	@Autowired
+	TRoleMapper tRoleMapper;
 
 	@RequestMapping("/selectOne")
 	@ResponseBody
 	public String selectOne() {
 
+		TRole role = new TRole();
+		role.setId("159ccce2265e04f4533378e0e6c28958");
+		role.setName("service");
+//		role.setDesc("service desc");
+
+		int code = tRoleMapper.insert(role);
+		System.out.println("role" + code);
 		return "success";
 	}
 
