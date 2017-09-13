5554deff43fc2a2a0aac49a05fa66eb89e0c0860
jiekekeji <jieke_rose@foxmail.com>
2017-09-13 09:53:11 +0800

    init

--- src/main/java/com/jk/ctrl/TRoleCtrl.java
+++ src/main/java/com/jk/ctrl/TRoleCtrl.java
@@ -1,5 +1,7 @@
 package com.jk.ctrl;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +20,14 @@ public class TRoleCtrl {
 	@RequestMapping("/selectOne")
 	@ResponseBody
 	public String selectOne() {
+		
+		String uuid=UUID.randomUUID().toString().replace("-", "").toLowerCase();
 
-//		TRole role = new TRole();
-//		role.setId("159ccce2265e04f4533378e0e6c28911");
-//		role.setName("service");
-////		role.setDesc("service desc");
-//
-//		int code = tRoleMapper.insert(role);
+		TRole role = new TRole();
+		role.setId(uuid);
+		role.setName("Asdfasdfasdf阿斯顿发阿斯顿发送阿斯顿发fsaffffffffff等待斗订单订单1");
+		role.setRemarks("备注信息");
+		int code = tRoleMapper.insert(role);
 		System.out.println("role" + tRoleMapper.selectByPrimaryKey("159ccce2265e04f4533378e0e6c28911"));
 		return "success";
 	}
