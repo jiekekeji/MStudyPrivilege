8f097f85f47cdd2aff16164a3ac0cd7bb1e95e8e
jiekekeji <jieke_rose@foxmail.com>
2017-09-29 11:26:40 +0800

    init

--- src/main/java/com/jk/filters/LoginFilter.java
+++ src/main/java/com/jk/filters/LoginFilter.java
@@ -8,15 +8,28 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 登录验证过滤器
+ * 
+ * @author Handsome
+ *
+ */
 public class LoginFilter implements Filter {
 
 	public void init(FilterConfig filterConfig) throws ServletException {
 
 	}
 
+	/**
+	 * 过滤逻辑
+	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		HttpServletRequest hsRequest = (HttpServletRequest) request;
+		String res = hsRequest.getRequestURI();
+		System.out.println("访问资源:" + res);
 
 	}
 
